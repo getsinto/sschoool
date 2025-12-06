6c5e955417375fae38196095f85e7ecf1f33be0d
devitpro <itsmtext@gmail.com>
2025-12-06 14:25:45 +0000

    fix: add 'use client' to SharedLayout and Footer components

--- components/layout/SharedLayout.tsx
+++ components/layout/SharedLayout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Header from '@/components/landing/Header'
 import Footer from '@/components/shared/Footer'
 
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import { Mail, Phone, MapPin, Facebook, Twitter, Instagram, Linkedin, Youtube } from 'lucide-react'
 
